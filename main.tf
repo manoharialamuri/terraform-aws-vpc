da96229c81a8b2555a526df6b17cf83fcaa43f46
manu <example@gmail.com>
2026-03-09 20:34:24 +0530

    terraform

--- main.tf
+++ main.tf
@@ -16,9 +16,9 @@ resource "aws_internet_gateway" "igw" {
 
 #creating public subnets
 resource "aws_subnet" "public" {
-  count = length(var.subnet_cidr)
+  count = length(var.public_subnet_cidr)
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet_cidr[count.index]
+  cidr_block = var.public_subnet_cidr[count.index]
   map_public_ip_on_launch = true
   availability_zone = local.az_names[count.index]
 
@@ -29,5 +29,83 @@ resource "aws_subnet" "public" {
         #name=roboshop-dev-public-us-east-1a
     },
     var.public_subnet_tags
+  )
+}
+
+#creating private subnets
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidr[count.index]
+  
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.enviornment}-private-${local.az_names[count.index]}"
+        #name=roboshop-dev-private-us-east-1a
+    },
+    var.private_subnet_tags
+  )
+}
+
+#creating database subnets
+resource "aws_subnet" "database" {
+  count = length(var.database_subnet_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidr[count.index]
+  
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.enviornment}-database-${local.az_names[count.index]}"
+        #name=roboshop-dev-database-us-east-1a
+    },
+    var.database_subnet_tags
+  )
+}
+
+#creating route table for public
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags =merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.enviornment}-public"
+        #name=roboshop-dev-public
+    },
+    var.public_route_table_tags
+  )
+}
+
+#creating route table for private
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags =merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.enviornment}-private"
+        #name=roboshop-dev-private
+    },
+    var.private_route_table_tags
+  )
+}
+
+#creating route table for database
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+
+  tags =merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.enviornment}-database"
+        #name=roboshop-dev-public
+    },
+    var.database_route_table_tags
   )
 }
--- variables.tf
+++ variables.tf
@@ -21,7 +21,7 @@ variable "igw_tags" {
     default = {}
 }
 
-variable "subnet_cidr" {
+variable "public_subnet_cidr" {
     type = list
     default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
@@ -29,4 +29,39 @@ variable "subnet_cidr" {
 variable "public_subnet_tags" {
     type = map
     default = {}
-}   
+}  
+
+variable "private_subnet_cidr" {
+    type = list
+    default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "private_subnet_tags" {
+    type = map
+    default = {}
+} 
+
+variable "database_subnet_cidr" {
+    type = list
+    default = ["10.0.21.0/24", "10.0.22.0/24"]
+}
+
+variable "database_subnet_tags" {
+    type = map
+    default = {}
+}
+
+variable "public_route_table_tags" {
+    type = map
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    type = map
+    default = {}
+}
+
+variable "database_route_table_tags" {
+    type = map
+    default = {}
+}
