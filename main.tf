05f743cc18f6b7a826f25b89f3f18230cbee1e5b
manu <example@gmail.com>
2026-03-09 21:05:21 +0530

    terraform

--- main.tf
+++ main.tf
@@ -146,7 +146,7 @@ resource "aws_nat_gateway" "main" {
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.example]
+  depends_on = [aws_internet_gateway.igw]
 }
 
 #creating private route
